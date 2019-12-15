2bb283a35aeae6eb73c416dc2295926a35d175de
Huang, Kuo-Chan (KC) <kc.huang@ge.com>
2019-12-15 12:57:39 -0800

    changed App.js tex

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	I love you I-Ling
+	Changed to Feature Branch
         </p>
         <a
           className="App-link"
